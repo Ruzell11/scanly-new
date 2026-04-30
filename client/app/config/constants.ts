3f45a52f7a46500c37f9daee9fda1e313699434c
Ruzell <johnruzell123@gmail.com>
2026-04-30 09:58:44 +0800

    clean up on api url fix block content error

--- client/app/config/constants.ts
+++ client/app/config/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = "http://157.230.217.195"
+export const API_URL = "/api"
--- client/next.config.ts
+++ client/next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-eslint: {
+  eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://157.230.217.195/:path*",
+      },
+    ];
   },
 };
 
